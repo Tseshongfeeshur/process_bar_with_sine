7cbbaf6661dbc01ac355916ecf5c958fa0abc62a
Ryan Yuan <ryanyuanzhehan@gmail.com>
2026-05-12 09:51:11 +0800

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:process_bar_with_sine/progress_bar.dart';
+import 'package:process_bar_with_sine/process_bar_with_sine.dart';
 
 void main() {
   runApp(const ExampleApp());
